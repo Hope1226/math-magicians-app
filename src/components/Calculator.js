24dd96a78cd0a5a6b18c09e6e1d5787ca051f927
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2021-11-26 04:50:44 +0500

    Add logic and evants

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,30 +1,54 @@
 import React, { Component } from 'react';
+import calculate from '../logic/calculate';
 
 /* eslint-disable react/prefer-stateless-function */
 class Calculator extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      next: 0,
+      total: 0,
+    };
+
+    // this.handleEvent = this.handleEvent.bind(this);
+    this.handleEvent = (e) => {
+      this.setState((prevState) => calculate(prevState, e.target.innerText));
+    };
+  }
+
   render() {
+    const { next, total } = this.state;
+    let output = 0;
+
+    if (next) {
+      output = next;
+    } else if (total) {
+      output = total;
+    }
+
     return (
       <div className="calculator">
-        <div className="display">0</div>
-        <div className="buttons functions">AC</div>
-        <div className="buttons functions">+/-</div>
-        <div className="buttons functions">%</div>
-        <div className="buttons operetors">÷</div>
-        <div className="buttons operands">7</div>
-        <div className="buttons operands">8</div>
-        <div className="buttons operands">9</div>
-        <div className="buttons operetors">x</div>
-        <div className="buttons operands">4</div>
-        <div className="buttons operands">5</div>
-        <div className="buttons operands">6</div>
-        <div className="buttons operetors">-</div>
-        <div className="buttons operands">1</div>
-        <div className="buttons operands">2</div>
-        <div className="buttons operands">3</div>
-        <div className="buttons operetors">+</div>
-        <div className="buttons operands two-span">0</div>
-        <div className="buttons operands">.</div>
-        <div className="buttons operetors">=</div>
+        <div className="display">{output}</div>
+        <button type="button" className="buttons functions" onClick={this.handleEvent}>AC</button>
+        <button type="button" className="buttons functions" onClick={this.handleEvent}>+/-</button>
+        <button type="button" className="buttons functions" onClick={this.handleEvent}>%</button>
+        <button type="button" className="buttons operetors" onClick={this.handleEvent}>÷</button>
+        <button type="button" className="buttons operands" onClick={this.handleEvent}>7</button>
+        <button type="button" className="buttons operands" onClick={this.handleEvent}>8</button>
+        <button type="button" className="buttons operands" onClick={this.handleEvent}>9</button>
+        <button type="button" className="buttons operetors" onClick={this.handleEvent}>x</button>
+        <button type="button" className="buttons operands" onClick={this.handleEvent}>4</button>
+        <button type="button" className="buttons operands" onClick={this.handleEvent}>5</button>
+        <button type="button" className="buttons operands" onClick={this.handleEvent}>6</button>
+        <button type="button" className="buttons operetors" onClick={this.handleEvent}>-</button>
+        <button type="button" className="buttons operands" onClick={this.handleEvent}>1</button>
+        <button type="button" className="buttons operands" onClick={this.handleEvent}>2</button>
+        <button type="button" className="buttons operands" onClick={this.handleEvent}>3</button>
+        <button type="button" className="buttons operetors" onClick={this.handleEvent}>+</button>
+        <button type="button" className="buttons operands two-span" onClick={this.handleEvent}>0</button>
+        <button type="button" className="buttons operands" onClick={this.handleEvent}>.</button>
+        <button type="button" className="buttons operetors" onClick={this.handleEvent}>=</button>
       </div>
     );
   }
