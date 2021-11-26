b4f61b18163a10e882cbb6b9ce7d6cfe5945b15b
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2021-11-26 22:22:19 +0500

    Refactor class based components wiht funtions

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,56 +1,37 @@
-import React, { Component } from 'react';
-import calculate from '../logic/calculate';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-/* eslint-disable react/prefer-stateless-function */
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      next: 0,
-      total: 0,
-    };
-
-    this.handleEvent = (e) => {
-      this.setState((prevState) => calculate(prevState, e.target.innerText));
-    };
-  }
-
-  render() {
-    const { next, total } = this.state;
-    let output = 0;
-
-    if (next) {
-      output = next;
-    } else if (total) {
-      output = total;
-    }
-
-    return (
-      <div className="calculator">
-        <div className="display">{output}</div>
-        <button type="button" className="buttons functions" onClick={this.handleEvent}>AC</button>
-        <button type="button" className="buttons functions" onClick={this.handleEvent}>+/-</button>
-        <button type="button" className="buttons functions" onClick={this.handleEvent}>%</button>
-        <button type="button" className="buttons operetors" onClick={this.handleEvent}>÷</button>
-        <button type="button" className="buttons operands" onClick={this.handleEvent}>7</button>
-        <button type="button" className="buttons operands" onClick={this.handleEvent}>8</button>
-        <button type="button" className="buttons operands" onClick={this.handleEvent}>9</button>
-        <button type="button" className="buttons operetors" onClick={this.handleEvent}>x</button>
-        <button type="button" className="buttons operands" onClick={this.handleEvent}>4</button>
-        <button type="button" className="buttons operands" onClick={this.handleEvent}>5</button>
-        <button type="button" className="buttons operands" onClick={this.handleEvent}>6</button>
-        <button type="button" className="buttons operetors" onClick={this.handleEvent}>-</button>
-        <button type="button" className="buttons operands" onClick={this.handleEvent}>1</button>
-        <button type="button" className="buttons operands" onClick={this.handleEvent}>2</button>
-        <button type="button" className="buttons operands" onClick={this.handleEvent}>3</button>
-        <button type="button" className="buttons operetors" onClick={this.handleEvent}>+</button>
-        <button type="button" className="buttons operands two-span" onClick={this.handleEvent}>0</button>
-        <button type="button" className="buttons operands" onClick={this.handleEvent}>.</button>
-        <button type="button" className="buttons operetors" onClick={this.handleEvent}>=</button>
-      </div>
-    );
-  }
+function Calculator(props) {
+  const { handleEvent, output } = props;
+  return (
+    <div className="calculator">
+      <div className="display">{output}</div>
+      <button type="button" className="buttons functions" onClick={handleEvent}>AC</button>
+      <button type="button" className="buttons functions" onClick={handleEvent}>+/-</button>
+      <button type="button" className="buttons functions" onClick={handleEvent}>%</button>
+      <button type="button" className="buttons operetors" onClick={handleEvent}>÷</button>
+      <button type="button" className="buttons operands" onClick={handleEvent}>7</button>
+      <button type="button" className="buttons operands" onClick={handleEvent}>8</button>
+      <button type="button" className="buttons operands" onClick={handleEvent}>9</button>
+      <button type="button" className="buttons operetors" onClick={handleEvent}>x</button>
+      <button type="button" className="buttons operands" onClick={handleEvent}>4</button>
+      <button type="button" className="buttons operands" onClick={handleEvent}>5</button>
+      <button type="button" className="buttons operands" onClick={handleEvent}>6</button>
+      <button type="button" className="buttons operetors" onClick={handleEvent}>-</button>
+      <button type="button" className="buttons operands" onClick={handleEvent}>1</button>
+      <button type="button" className="buttons operands" onClick={handleEvent}>2</button>
+      <button type="button" className="buttons operands" onClick={handleEvent}>3</button>
+      <button type="button" className="buttons operetors" onClick={handleEvent}>+</button>
+      <button type="button" className="buttons operands two-span" onClick={handleEvent}>0</button>
+      <button type="button" className="buttons operands" onClick={handleEvent}>.</button>
+      <button type="button" className="buttons operetors" onClick={handleEvent}>=</button>
+    </div>
+  );
 }
+
+Calculator.propTypes = {
+  output: PropTypes.instanceOf(Object).isRequired,
+  handleEvent: PropTypes.func.isRequired,
+};
 
 export default Calculator;
