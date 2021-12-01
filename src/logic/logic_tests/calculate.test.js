8d95360e0588eb7f8262aefa6fe22527e7d47445
VanessaAoki <66967274+VanessaAoki@users.noreply.github.com>
2021-12-01 18:16:10 -0300

    Add more tests for the calculate logic

--- src/logic/logic_tests/calculate.test.js
+++ src/logic/logic_tests/calculate.test.js
@@ -95,4 +95,48 @@ describe('Test calculates logics in different use cases', () => {
       operation: null,
     });
   });
+
+  it('Test case user presses an operation after pressing "=": calculate(intObj, "+") total = 10 should return operation +', () => {
+    intObj.next = null;
+    intObj.operation = null;
+    intObj.total = '10';
+    expect(calculate(intObj, '+')).toEqual({
+      total: '10',
+      next: null,
+      operation: '+',
+    });
+  });
+
+  it('Test case user presses an operation button and there is an existing operation: calculate(intObj, "-") total = 5 should return operation -', () => {
+    intObj.next = null;
+    intObj.operation = '+';
+    intObj.total = '5';
+    expect(calculate(intObj, '-')).toEqual({
+      total: '5',
+      next: null,
+      operation: '-',
+    });
+  });
+
+  it('Test case user presses an operation button and there is an existing operation: calculate(intObj, "+") next should be 1, total should be 5, and it must return new total 6', () => {
+    intObj.next = '1';
+    intObj.operation = '+';
+    intObj.total = '5';
+    expect(calculate(intObj, '+')).toEqual({
+      total: '6',
+      next: null,
+      operation: '+',
+    });
+  });
+
+  it('Test case user user hasn\'t typed a number yet, just the operation: calculate(intObj, "+") total should be 0, and it must return operation +', () => {
+    intObj.next = null;
+    intObj.operation = null;
+    intObj.total = '0';
+    expect(calculate(intObj, '+')).toEqual({
+      next: null,
+      operation: '+',
+      total: '0',
+    });
+  });
 });
